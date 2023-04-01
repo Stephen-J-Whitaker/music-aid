bd621126aff1931556f197e656b5342945ab812d
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-04-01 08:26:26 +0000

    FEAT: Register Song model with admin

--- songbook/admin.py
+++ songbook/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Song
 
-# Register your models here.
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'song_slug')
+    search_fields = ['title', 'lyrics']
+    prepopulated_fields = {'song_slug': ('user', 'title',)}
