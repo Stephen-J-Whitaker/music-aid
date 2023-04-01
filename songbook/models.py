1074f74a128725461fb4456a947b662c2a0c5362
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-04-01 08:16:18 +0000

    Add Song model

--- songbook/models.py
+++ songbook/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Song(models.Model):
+    """
+    Model to hold song and lyric details
+    """
+    title = models.CharField(max_length=150, unique=True)
+    artist = models.CharField(max_length=150, unique=True)
+    lyrics = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name="user_songs")
+    song_slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['title'],
+                                    name='unique_user_song_title')
+        ]
+
+    def __str__(self):
+        return self.title
