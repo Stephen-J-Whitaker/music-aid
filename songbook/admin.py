2827aae757d4ded3c574abe7f5192ab91a622fff
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-04-03 10:28:30 +0000

    FEAT: Add setlist to admin page

--- songbook/admin.py
+++ songbook/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from .models import Song
+from .models import Song, Setlist
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Song)
 class SongAdmin(SummernoteModelAdmin):
+    """
+    Song admin registration class
+    """
     list_display = ('user', 'title', 'artist')
     search_fields = ['title', 'lyrics']
     summernote_fields = ('lyrics')
+
+@admin.register(Setlist)
+class SetlistAdmin(admin.ModelAdmin):
+    """
+    Setlist admin registration class
+    """
+    list_display = ('user', 'setlist_name')
