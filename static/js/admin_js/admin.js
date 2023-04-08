cac8df8c73ed14462ae7dde1aafe6fad2eba47b4
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-04-08 05:40:05 +0000

    FEAT: Create admin js file for pasted lyric style removal

---
+++ static/js/admin_js/admin.js
@@ -0,0 +1,28 @@
+/**
+ * Js script for music aid admin page
+ */
+
+
+/**
+ * Add functions for page manipulation on page load completion
+ */
+document.addEventListener('DOMContentLoaded', function() {
+
+    /**
+     * Function to intercept paste events into summernote 
+     * and remove all formatting and html.
+     * Plain text is returned to the summernote window.
+     */
+    if (window[0]) {
+        window[0].addEventListener('paste', function(event) {
+            let inputText = (event.clipboardData || window.clipboardData).getData('text/plain');
+            event.preventDefault();
+            let summernoteIframe = document.getElementById('id_lyrics_iframe')
+            let innerDoc = summernoteIframe.contentDocument || summernoteIframe.contentWindow.document;
+            let frameContent = innerDoc.getElementsByClassName('note-editable')
+            frameContent[0].innerHTML = inputText
+            frameContent[0].innerText = inputText
+        });
+    };
+
+});
