b96d6437a401fd323bc5a8b3202754276f08a2d3
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-04-08 04:20:47 +0000

    FEAT: Move messages js into a common messages js file

---
+++ static/js/songbook_js/songbook_messages.js
@@ -0,0 +1,20 @@
+/**
+ * Js script for music aid songbook app messages
+ */
+
+/**
+ * Add functions for page manipulation on page load completion
+ */
+document.addEventListener('DOMContentLoaded', function() {
+ 
+    // Set timeout for messages code provided by Code Institute
+    if (document.getElementById('msg')) {
+        setTimeout(function () {
+            let messages = document.getElementById('msg');
+            let alert = new bootstrap.Alert(messages);
+            alert.close();
+        }, 3000);
+    };
+    // End of set timeout for messages code provided by Code Institute
+
+});
