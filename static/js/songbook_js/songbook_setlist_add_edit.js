c6788ea313e6608f63d6a4c22c6a7fa1823f8dcf
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-04-12 16:54:22 +0000

    FIX: Lint and correct song_add_edit.js as necessary

--- static/js/songbook_js/songbook_setlist_add_edit.js
+++ static/js/songbook_js/songbook_setlist_add_edit.js
@@ -7,87 +7,89 @@
  * Add functions for page manipulation on page load completion
  */
 document.addEventListener('DOMContentLoaded', function() {
-        /**
-         * Use ajax to check if input entry is unique for the user
-         */
-        let originalSetlistName = $('#id_setlist_name').val();
-    
-        let setlistName = $('#id_setlist_name').val();
-        let trimmedSetlistName = setlistName.trim();
-        $('#id_setlist_name').val(trimmedSetlistName);
+    /**
+     * Use ajax to check if input entry is unique for the user
+     */
 
-        let setlistNameLabel = document.querySelector('label[for="id_setlist_name"]');
+    // Actions to be taken on completion of form page load
+    let originalSetlistName = $('#id_setlist_name').val();
 
-        setlistNameLabel.innerHTML += '<div class="note">Note: leading, trailing and multiple spaces in a row are removed</div>';
+    let setlistName = $('#id_setlist_name').val();
+    let trimmedSetlistName = setlistName.trim();
+    $('#id_setlist_name').val(trimmedSetlistName);
 
-        function validateUnique() {
-            if ($('#id_setlist_name').val() != '') {
-                /* globals $ */
-                setlistName = $('#id_setlist_name').val();
-                // Replace regex for multi white space removal sourced at:
-                // https://www.tutorialrepublic.com/faq/how-to-replace-multiple-spaces-with-single-space-in-javascript.php
-                trimmedSetlistName = setlistName.trimStart().replace(/  +/g, ' ');
-                // $('#id_setlist_name').val(trimmedSetlistName);
-                setlistName = trimmedSetlistName.trimEnd();
-                $.ajax(
-                {
-                    type:'GET',
-                    url: '/setlist_name_validate',
-                    data:{
-                        setlist_name: setlistName
-                    },
-                    success: function(data) {
-                        if (data === 'in_use') {
-                            if ($('#id_setlist_name').val().trimEnd() === originalSetlistName) {
-                                $('#id_setlist_name').css('background-color', 'white');
-                                setlistNameLabel.classList.remove('setlist-name-status');
-                                $('#setlist-submit-btn').show();
-                            } else {
-                                $('#id_setlist_name').css('background-color', 'rgb(255, 137, 137)');
-                                setlistNameLabel.classList.add('setlist-name-status');
-                                $('#setlist-submit-btn').hide();
-                            }
-                        } else {
+    let setlistNameLabel = document.querySelector('label[for="id_setlist_name"]');
+    setlistNameLabel.innerHTML += '<div class="note">Note: leading, trailing and multiple spaces in a row are removed</div>';
+    // End of actions to be taken on completion of form page load
+
+    function validateUnique() {
+        if ($('#id_setlist_name').val() != '') {
+            /* globals $ */
+            setlistName = $('#id_setlist_name').val();
+            // Replace regex for multi white space removal sourced at:
+            // https://www.tutorialrepublic.com/faq/how-to-replace-multiple-spaces-with-single-space-in-javascript.php
+            trimmedSetlistName = setlistName.trimStart().replace(/  +/g, ' ');
+            // $('#id_setlist_name').val(trimmedSetlistName);
+            setlistName = trimmedSetlistName.trimEnd();
+            $.ajax(
+            {
+                type:'GET',
+                url: '/setlist_name_validate',
+                data:{
+                    setlist_name: setlistName
+                },
+                success: function(data) {
+                    if (data === 'in_use') {
+                        if ($('#id_setlist_name').val().trimEnd() === originalSetlistName) {
                             $('#id_setlist_name').css('background-color', 'white');
                             setlistNameLabel.classList.remove('setlist-name-status');
                             $('#setlist-submit-btn').show();
+                        } else {
+                            $('#id_setlist_name').css('background-color', 'rgb(255, 137, 137)');
+                            setlistNameLabel.classList.add('setlist-name-status');
+                            $('#setlist-submit-btn').hide();
                         }
+                    } else {
+                        $('#id_setlist_name').css('background-color', 'white');
+                        setlistNameLabel.classList.remove('setlist-name-status');
+                        $('#setlist-submit-btn').show();
                     }
-                });
-            }
+                }
+            });
         }
+    }
 
-        /**
-         * Switch off submit event listener and submit form
-         */
-        function submitForm() {
-            if ($('#setlist-submit-btn').is(':visible')) {
-                $('form').off('submit');
-                $('form').submit();
-            }
+    /**
+     * Switch off submit event listener and submit form
+     */
+    function submitForm() {
+        if ($('#setlist-submit-btn').is(':visible')) {
+            $('form').off('submit');
+            $('form').submit();
         }
-        
-        validateUnique();
-        $('#id_setlist_name').keyup('input', validateUnique);
-        $('#id_setlist_name').change(validateUnique);
+    }
     
-        /**
-         * Trim any trailing white spaces before revalidation
-         * and submit form only if save button has not been removed by
-         * validation
-         */
-        $('form').submit(function(thisEvent) {
+    validateUnique();
+    $('#id_setlist_name').keyup('input', validateUnique);
+    $('#id_setlist_name').change(validateUnique);
 
-            thisEvent.preventDefault();
-            setlistName = $('#id_setlist_name').val();
-            trimmedSetlistName = setlistName.trimEnd();
-            // Replace regex for multi white space removal sourced at:
-            // https://www.tutorialrepublic.com/faq/how-to-replace-multiple-spaces-with-single-space-in-javascript.php
-            trimmedSetlistName = trimmedSetlistName.replace(/  +/g, ' ');
-            $('#id_setlist_name').val(trimmedSetlistName);
-            setTimeout(validateUnique, 100);
+    /**
+     * Trim any trailing white spaces before revalidation
+     * and submit form only if save button has not been removed by
+     * validation
+     */
+    $('form').submit(function(thisEvent) {
 
-            setTimeout(submitForm, 300);
-        });
+        thisEvent.preventDefault();
+        setlistName = $('#id_setlist_name').val();
+        trimmedSetlistName = setlistName.trimEnd();
+        // Replace regex for multi white space removal sourced at:
+        // https://www.tutorialrepublic.com/faq/how-to-replace-multiple-spaces-with-single-space-in-javascript.php
+        trimmedSetlistName = trimmedSetlistName.replace(/  +/g, ' ');
+        $('#id_setlist_name').val(trimmedSetlistName);
+        setTimeout(validateUnique, 100);
 
+        setTimeout(submitForm, 300);
     });
+
+});
--- static/js/songbook_js/songbook_song_add_edit.js
+++ static/js/songbook_js/songbook_song_add_edit.js
@@ -15,117 +15,102 @@ document.addEventListener('DOMContentLoaded', function() {
      * Code inspired by the following post:
      * https://stackoverflow.com/questions/71334716
      */
-    if (window[0]) {
-        window[0].addEventListener('paste', function(event) {
-            let inputText = (event.clipboardData || window.clipboardData).getData('text/plain');
-            event.preventDefault();
-            let summernoteIframe = document.getElementById('id_lyrics_iframe')
-            let innerDoc = summernoteIframe.contentDocument || summernoteIframe.contentWindow.document;
-            let frameContent = innerDoc.getElementsByClassName('note-editable');
-            let frameHTML =  frameContent[0].innerHTML;
-            let frameText = frameContent[0].innerText;
-            frameHTML += inputText;
-            frameText += inputText;
-            frameContent[0].innerHTML = frameHTML;
-            frameContent[0].innerText = frameText;
-        });
-    };
+    window[0].addEventListener('paste', function(event) {
+        let inputText = (event.clipboardData || window.clipboardData).getData('text/plain');
+        event.preventDefault();
+        let summernoteIframe = document.getElementById('id_lyrics_iframe');
+        let innerDoc = summernoteIframe.contentDocument || summernoteIframe.contentWindow.document;
+        let frameContent = innerDoc.getElementsByClassName('note-editable');
+        let frameHTML =  frameContent[0].innerHTML;
+        let frameText = frameContent[0].innerText;
+        frameHTML += inputText;
+        frameText += inputText;
+        frameContent[0].innerHTML = frameHTML;
+        frameContent[0].innerText = frameText;
+    });
 
-    if (document.getElementById('id_title')) {
-        /**
-         * Use ajax to check if input entry is unique for the user
-         */
+    /**
+     * Use ajax to check if input entry is unique for the user
+     */
+    // Actions to be taken on completion of form page load
+    let originalTitle = $('#id_title').val();
 
-        let originalTitle = $('#id_title').val()
-        console.log('original title', originalTitle)
-    
-        let songTitle = $('#id_title').val();
-        let trimmedTitle = songTitle.trim();
-        $('#id_title').val(trimmedTitle);
-        console.log('trimmed title', $('#id_title').val());
+    let songTitle = $('#id_title').val();
+    let trimmedTitle = songTitle.trim();
+    $('#id_title').val(trimmedTitle);
 
-        let titleLabel = document.querySelector('label[for="id_title"]')
+    let titleLabel = document.querySelector('label[for="id_title"]');
+    titleLabel.innerHTML += '<div class="note">Note: leading, trailing and multiple spaces in a row are removed</div>';
+    // End of actions to be taken on completion of form page load
 
-        titleLabel.innerHTML += '<div class="note">Note: leading, trailing and multiple spaces in a row are removed</div>';
-
-        function validateUnique() {
-            console.log('in validate unique')
-            if ($('#id_title').val() != '') {
-                songTitle = $('#id_title').val();
-                // Replace regex for multi white space removal sourced at:
-                // https://www.tutorialrepublic.com/faq/how-to-replace-multiple-spaces-with-single-space-in-javascript.php
-                trimmedTitle = songTitle.trimStart().replace(/  +/g, ' ');
-                // $('#id_title').val(trimmedTitle);
-                songTitle = trimmedTitle.trimEnd();
-                
-                $.ajax(
-                {
-                    type:'GET',
-                    url: '/song_title_validate',
-                    data:{
-                        song_title: songTitle
-                    },
-                    success: function(data) {
-                        console.log('data', data);
-                        if (data === 'in_use') {
-                            if ($('#id_title').val().trimStart().trimEnd().replace(/  +/g, ' ') === originalTitle) {
-                                $('#id_title').css('background-color', 'white');
-                                titleLabel.classList.remove('title-status');
-                                $('#song-submit-btn').show();
-                                console.log('in validate unique oriinal title')
-                            } else {
-                                $('#id_title').css('background-color', 'rgb(255, 137, 137)');
-                                titleLabel.classList.add('title-status');
-                                $('#song-submit-btn').hide();
-                                console.log('in validate unique in use')
-                            }
-                        } else {
+    function validateUnique() {
+        if ($('#id_title').val() != '') {
+            /* globals $ */
+            songTitle = $('#id_title').val();
+            // Replace regex for multi white space removal sourced at:
+            // https://www.tutorialrepublic.com/faq/how-to-replace-multiple-spaces-with-single-space-in-javascript.php
+            trimmedTitle = songTitle.trimStart().replace(/  +/g, ' ');
+            // $('#id_title').val(trimmedTitle);
+            songTitle = trimmedTitle.trimEnd();
+            
+            $.ajax(
+            {
+                type:'GET',
+                url: '/song_title_validate',
+                data:{
+                    song_title: songTitle
+                },
+                success: function(data) {
+                    if (data === 'in_use') {
+                        if ($('#id_title').val().trimStart().trimEnd().replace(/  +/g, ' ') === originalTitle) {
                             $('#id_title').css('background-color', 'white');
                             titleLabel.classList.remove('title-status');
                             $('#song-submit-btn').show();
-                            console.log('in validate unique available')
+                        } else {
+                            $('#id_title').css('background-color', 'rgb(255, 137, 137)');
+                            titleLabel.classList.add('title-status');
+                            $('#song-submit-btn').hide();
                         }
+                    } else {
+                        $('#id_title').css('background-color', 'white');
+                        titleLabel.classList.remove('title-status');
+                        $('#song-submit-btn').show();
                     }
-                })
-            }
+                }
+            });
         }
-
-        /**
-         * Switch off submit event listener and submit form
-         */
-        function submitForm() {
-            if ($('#song-submit-btn').is(':visible')) {
-                $('form').off('submit');
-                $('form').submit();
-            };
-        }
-      
-        validateUnique();
-        $('#id_title').keyup('input', validateUnique);
-        $('#id_title').change(validateUnique);
-
-        /**
-         * Trim any trailing white spaces before revalidation
-         * and submit form only if save button has not been removed by
-         * validation
-         */
-        $('form').submit(function(thisEvent) {
-
-            console.log('in submit js')
-            thisEvent.preventDefault();
-            songTitle = $('#id_title').val();
-            trimmedTitle = songTitle.trimEnd();
-            console.log('trim end ', trimmedTitle);
-            // Replace regex for multi white space removal sourced at:
-            // https://www.tutorialrepublic.com/faq/how-to-replace-multiple-spaces-with-single-space-in-javascript.php
-            trimmedTitle = trimmedTitle.replace(/  +/g, ' ');
-            console.log('trim middle ', trimmedTitle);
-            $('#id_title').val(trimmedTitle);
-            setTimeout(validateUnique, 100);
-
-            setTimeout(submitForm, 300);
-        })
-
     }
 
+    /**
+     * Switch off submit event listener and submit form
+     */
+    function submitForm() {
+        if ($('#song-submit-btn').is(':visible')) {
+            $('form').off('submit');
+            $('form').submit();
+        }
+    }
+    
+    validateUnique();
+    $('#id_title').keyup('input', validateUnique);
+    $('#id_title').change(validateUnique);
+
+    /**
+     * Trim any trailing white spaces before revalidation
+     * and submit form only if save button has not been removed by
+     * validation
+     */
+    $('form').submit(function(thisEvent) {
+
+        thisEvent.preventDefault();
+        songTitle = $('#id_title').val();
+        trimmedTitle = songTitle.trimEnd();
+        // Replace regex for multi white space removal sourced at:
+        // https://www.tutorialrepublic.com/faq/how-to-replace-multiple-spaces-with-single-space-in-javascript.php
+        trimmedTitle = trimmedTitle.replace(/  +/g, ' ');
+        $('#id_title').val(trimmedTitle);
+        setTimeout(validateUnique, 100);
+
+        setTimeout(submitForm, 300);
+    });
 });
