0c6b362fc25238fd7a8beb1d5cb4074a208876ac
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-04-16 19:51:51 +0000

    DOCS: Add Credits and Acknowledgements sections to readme

--- music_aid/urls.py
+++ music_aid/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('songbook.urls'), name='songbook_urls'),
+    # Configuration supplied by Code Institute
     path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
+    # End of configuration supplied by Code Institute
 ]
